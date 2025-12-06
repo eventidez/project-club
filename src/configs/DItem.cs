81733150f65adae8610decae19b5fb66bde628cd
EZZZ <eventidez0920@yeah.net>
2025-12-06 23:22:39 +0800

    data

--- src/configs/DItem.cs
+++ src/configs/DItem.cs
@@ -2,7 +2,7 @@ using Godot;
 
 namespace Game;
 
-public partial class DataItem : DataBase
+public partial class DItem : DataBase
 {
     [Export] public int Money { get; set; }
     [Export] public string SettingName { get; set; }
--- src/configs/DSetting.cs
+++ src/configs/DSetting.cs
@@ -2,7 +2,7 @@ using Godot;
 
 namespace Game;
 
-public partial class DataSetting : DataBase
+public partial class DSetting : DataBase
 {
     [Export] public string Type { get; set; }
     [Export] public string InitialValue { get; set; }
